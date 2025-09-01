32a13a5456eadcf93776eefbb140e1843320c273
NatZigaran. <natanaelzigaran@gmail.com>
2025-09-01 19:45:39 -0300

    Se creo el modelo de profile

---
+++ src/models/profile.model.js
@@ -0,0 +1,33 @@
+import sequelize from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+const ProfileModel = sequelize.define(
+  "profile",
+  {
+    first_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    biography: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    avatar_url: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    birth_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+export default ProfileModel;
