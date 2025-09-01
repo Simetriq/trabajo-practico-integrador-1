4e456a3a1ff105c87e46958de5ffbebd95e79eea
NatZigaran. <natanaelzigaran@gmail.com>
2025-09-01 19:51:34 -0300

    Se creo el modelo de article

---
+++ src/models/article.model.js
@@ -0,0 +1,29 @@
+import sequelize from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+const ArticleModel = sequelize.define(
+  "Article",
+
+  {
+    title: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    excerpt: {
+      type: DataTypes.STRING(500),
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.ENUM("published", "archived"),
+      defaultValue: "published",
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
