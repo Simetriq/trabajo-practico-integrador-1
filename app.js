9ff5d0ae64755fec54d49bcd3a25bd40ff8673a8
NatZigaran. <natanaelzigaran@gmail.com>
2025-09-04 20:22:41 -0300

    Se actualizaron los comentarios

--- app.js
+++ app.js
@@ -13,11 +13,20 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/api", routesUser);
 app.use("/api", routesProfile);
 app.use("/api", routesTag);
 app.use("/api", routesArticle);
 app.use("/api", routesArticleTag);
+app.use("/api", authRoutes);
 
 testDB().then(() => {
   app.listen(PORT, () => {
